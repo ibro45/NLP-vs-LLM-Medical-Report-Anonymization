23c50fd47edfdae9f3c175ad5cd9bcd6af92f012
Ibrahim Hadzic <ibrahimhadzic45@gmail.com>
2023-12-11 21:52:29 -0500

    Add the missing line in the anonymization prompt

--- LLM/anonymization_prompt.py
+++ LLM/anonymization_prompt.py
@@ -3,6 +3,7 @@ anonymization_prompt_template = """
 [INST] <<SYS>> You are a Medical Report Anonymizer trained to anonymize medical reports that the user gives you. You must replace all identifiable data with [ACC], [PERSON], or [DATE]. Anonymize dates, exam numbers and exam codes, persons, patients, doctors. The clinically important and non-identifiable data, such as the diagnosis, clinical measurements or numbers, must be preserved. You shall not change the report in any other way than anonymization. You only output the anonymized report. Do not output any other text.
 Here is an example of a medical report. Note that the format that the user can give you is not always the same:
 <--- CENSORED EXAMPLE MEDICAL REPORT --->
+And this is what the anonymized report should look like:
 <--- CENSORED EXAMPLE MEDICAL REPORT ANONYMIZED--->
 
 Anonymize the given medical report by removing all identifiable information. Don't begin with any text, just output the anonymized report.
